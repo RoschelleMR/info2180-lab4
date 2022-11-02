b6955bc8833fdc37a11a5bf79b4c75eb25a4f303
RoschelleMR <roschelle2k6@gmail.com>
2022-11-02 16:59:23 -0500

    Implemented AJAX call for superheroes.php

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 window.addEventListener("load", function () {
   let btn = document.querySelector(".btn");
 
+  const xhttp = new XMLHttpRequest(); //Ajax Request for superheroes.php
+
   btn.addEventListener("click", function () {
-    alert();
+    let url = "superheroes.php";
+    xhttp.onreadystatechange = loadList;
+    xhttp.open("GET", url);
+    xhttp.send();
   });
+
+  function loadList() {
+    if (xhttp.readyState === XMLHttpRequest.DONE) {
+      if (xhttp.status === 200) {
+        let response = xhttp.responseText;
+        alert(response);
+      } else {
+        alert("There's a problem with the request");
+      }
+    }
+  }
 });
